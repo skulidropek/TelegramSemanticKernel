ded874ad18b6e25157615daa646307f02ebcff7b
skulidropek <66840575+skulidropek@users.noreply.github.com>
2024-03-02 19:57:41 +0400

    Add project files.

---
+++ Extensions/TelegramExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramSemanticKernel.Services;
+
+namespace TelegramSemanticKernel.Extensions
+{
+    public static class TelegramExtensions
+    {
+        public static void AddTelegram(this IServiceCollection services, string token)
+        {
+            services.AddSingleton<ITelegramBotClient>(s => new TelegramBotClient(token));
+            services.AddSingleton<TelegramBotService>();
+        }
+
+        public static async Task<string> SendStreamingChatMessageContentsAsync(this ITelegramBotClient client, Chat chat, IAsyncEnumerable<StreamingChatMessageContent> result, string firstMessage = "Generate....", CancellationToken cancellationToken = default)
+        {
+            var message = await client.SendTextMessageAsync(chat, firstMessage, cancellationToken: cancellationToken);
+
+            await client.SendChatActionAsync(chat, ChatAction.Typing, cancellationToken: cancellationToken);
+
+            var fullMessage = "";
+            var oldMessage = "";
+            var cellMssage = "";
+
+            await foreach (var content in result)
+            {
+                cellMssage += content.Content;
+
+                if (cellMssage.Length > 150)
+                {
+                    fullMessage += cellMssage;
+                    oldMessage = fullMessage;
+                    cellMssage = "";
+                    message = await client.EditMessageTextAsync(chat, message.MessageId, fullMessage, cancellationToken: cancellationToken);
+                    await client.SendChatActionAsync(chat, ChatAction.Typing, cancellationToken: cancellationToken);
+                }
+            }
+
+            if (cellMssage.Length > 0)
+            {
+                fullMessage += cellMssage;
+            }
+
+            if (oldMessage != fullMessage)
+            {
+                await client.EditMessageTextAsync(chat, message.MessageId, fullMessage, cancellationToken: cancellationToken);
+            }
+
+            return fullMessage;
+        }
+    }
+}
---
+++ Services/TelegramBotService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Polling;
+using Telegram.Bot;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using System.Threading;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel.TextToImage;
+
+namespace TelegramSemanticKernel.Services
+{
+    public class TelegramBotService(ITelegramBotClient telegramBotClient, IChatCompletionService chatService, ILogger<TelegramBotService> logger)
+    {
+        //if (update.Message.Document != null)
+        //{
+        //    var file = await telegramBotClient.GetFileAsync(update.Message.Document.FileId, cancellationToken: cancellationToken);
+
+        //    string saveFilePath = $"{update.Message.Chat.Id}/{update.Message.Document.FileName}";
+
+        //    if (!Directory.Exists(update.Message.Chat.Id.ToString()))
+        //    {
+        //        Directory.CreateDirectory(update.Message.Chat.Id.ToString());
+        //    }
+
+        //    using (var saveFileStream = System.IO.File.Create(saveFilePath))
+        //    {
+        //        // Скачивание файла
+        //        await telegramBotClient.DownloadFileAsync(file.FilePath, saveFileStream, cancellationToken: cancellationToken);
+
+        //        string fileContent = await System.IO.File.ReadAllTextAsync(saveFilePath, cancellationToken);
+        //        chatHistory.AddUserMessage(fileContent);
+        //    }
+        //}
+
+        public void ChatStart(ReceiverOptions receiverOptions, Func<ITelegramBotClient, Update, CancellationToken, Task> handleUpdateAsync, CancellationToken cancellationToken = default)//
+        {
+
+            telegramBotClient.StartReceiving(
+                handleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );  
+        }
+
+        private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            logger.LogError($"An error occurred {exception.Message}", exception);
+            return Task.CompletedTask;
+        }
+    }
+}
